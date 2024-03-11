53c27bfb24f7a4f701325b2b0f6e288e552ae3d5
cagritukenmez <117526414+cagritukenmez@users.noreply.github.com>
2024-03-11 14:12:11 +0300

    Create average note calculator.java

---
+++ java101/practice/average note calculator.java
@@ -0,0 +1,30 @@
+package pck;
+import java.util.Scanner;
+public class DenemeSinifi {
+    public static void main(String[] args) {
+        int mathematics, physics, chemistry, turkish, history, music;
+        Scanner inp = new Scanner(System.in);
+
+        System.out.println("math note :");
+        mathematics =inp.nextInt();
+
+        System.out.println("physics note :");
+        physics = inp.nextInt();
+
+        System.out.println("chemistry note :");
+        chemistry = inp.nextInt();
+
+        System.out.println("turkish note :");
+        turkish = inp.nextInt();
+
+        System.out.println("history note :");
+        history = inp.nextInt();
+
+        System.out.println("music note :");
+        music = inp.nextInt();
+
+        float average = (mathematics + physics + chemistry + turkish + history+music)/6f;
+        System.out.println("your average note is :" + average );
+
+    }
+}
