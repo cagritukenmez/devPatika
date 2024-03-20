f8505f86d56fce2e286d3c65da576a1c2e7c0e20
cagritukenmez <117526414+cagritukenmez@users.noreply.github.com>
2024-03-20 17:38:36 +0300

    Create countPowOfValue.java

---
+++ java101/practice/countPowOfValue.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+public class Solution {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int value = input.nextInt();
+        int countPowOfFour=0;
+        int countPowOfFive=0;
+        int valueCover = value;
+        while(value %4==0){
+            value/=4;
+            countPowOfFour++;
+        }
+        value=valueCover;
+        while(value%5==0){
+            value/=5;
+            countPowOfFive++;
+        }
+        System.out.print("pow of four : " +countPowOfFour+" pow of five : "+countPowOfFive);
+    }
+}
